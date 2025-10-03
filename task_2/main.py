3c872b38453fc4a8e87e7302440d1722bec50c27
Дарья <dteleshevskaya.internet.ru>
2025-10-03 11:10:56 +0300

    tsk_2

---
+++ task_2/main.py
@@ -0,0 +1,5 @@
+k = int(input('Количесвто яблок: '))
+n = int(input('Количесвто школьнsиков: '))
+everyshk_yablk = k // n
+korzina = k % n
+print("На каждого школьника столько яблок: ", everyshk_yablk, "В корзине останется: ", korzina )
